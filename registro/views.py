b7ac2ef8764cd75b87ef9ff93409d9c5821111d6
Calles98 <65507456+Calles98@users.noreply.github.com>
2020-12-03 18:12:04 -0700

    Update views.py

--- registro/views.py
+++ registro/views.py
@@ -4,7 +4,16 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
+from registro.models import UserProfileInfo
+from home import views
 # Create your views here.
+
+
+
+@login_required
+def logout_usuario(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
 
 
 def registro(request):
@@ -14,40 +23,41 @@ def registro(request):
 
         if user_form.is_valid():
 
-            #user = user_form.save()
-            #user.set_password(user.password)
-            user_form.save()
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+
 
             registered = True
+
         else:
             print(user_form.errors)
     else:
         user_form = UserForm()
 
+
     return render(request,'registro/registro.html',{'user_form':user_form,'registered':registered})
 
 
 
+def login_usuario(request):
 
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
 
+        user = authenticate(username=username, password=password)
 
-# def registro(request):
-#     registered = False
-#     if request.method == "POST":
-#         user_form = UserForm(data=request.POST)
-#
-#         if user_form.is_valid():
-#
-#             user = user_form.save()
-#             user.set_password(user.password)
-#             user.save()
-#
-#             registered = True
-#
-#         else:
-#             print(user_form.errors)
-#     else:
-#         user_form = UserForm()
-#         #profile_form = UserProfileInfoForm()
-#
-#     return render(request,'registro/registro.html',{'user_form':user_form,'registered':registered})
+        if user:
+            if user.is_active:
+                login(request,user)
+                return HttpResponseRedirect(reverse('index'))
+            else:
+                return HttpResponse('CUENTA NO ACTIVA')
+
+        else:
+            print("Alguien intentó ingresar y falló")
+            print("Username: {} and password {}".format(username,password))
+            return HttpResponse("Datos invalidos para login")
+    else:
+        return render(request,'registro/login.html', {})
